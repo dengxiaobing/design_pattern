e9a2f90a5368b806eeaa59082686d1b76ddc8c31
dengxiaobing <657563945dxb@gmail.com>
2020-06-26 10:39:38 +0800

    序列号破坏单例模式原理

--- src/main/java/cn/niudehua/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/cn/niudehua/design/pattern/creational/singleton/HungrySingleton.java
@@ -1,11 +1,13 @@
 package cn.niudehua.design.pattern.creational.singleton;
 
+import java.io.Serializable;
+
 /**
  * @author: deng
  * @datetime: 2020/6/23 9:47 下午
  * @desc:
  */
-public class HungrySingleton {
+public class HungrySingleton implements Serializable {
     private static HungrySingleton hungrySingleton = null;
 
     static {
@@ -13,10 +15,14 @@ public class HungrySingleton {
     }
 
     private HungrySingleton() {
-
     }
 
     public static HungrySingleton getInstance() {
+        return hungrySingleton;
+    }
+
+
+    public Object readResolve() {
         return hungrySingleton;
     }
 }
--- src/main/java/cn/niudehua/design/pattern/creational/singleton/Test.java
+++ src/main/java/cn/niudehua/design/pattern/creational/singleton/Test.java
@@ -1,25 +1,43 @@
 package cn.niudehua.design.pattern.creational.singleton;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * @author: deng
  * @datetime: 2020/6/22 10:33 下午
  * @desc:
  */
 public class Test {
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(new T());
-        Thread thread2 = new Thread(new T());
-        Thread thread3 = new Thread(new T());
-        Thread thread4 = new Thread(new T());
-        Thread thread5 = new Thread(new T());
-        Thread thread6 = new Thread(new T());
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
-        thread6.start();
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+//        Thread thread1 = new Thread(new T());
+//        Thread thread2 = new Thread(new T());
+//        Thread thread3 = new Thread(new T());
+//        Thread thread4 = new Thread(new T());
+//        Thread thread5 = new Thread(new T());
+//        Thread thread6 = new Thread(new T());
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
+//        thread5.start();
+//        thread6.start();
+//
+//        System.out.println("program end");
 
-        System.out.println("main Thread end");
+        HungrySingleton instance = HungrySingleton.getInstance();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("singleton_file"));
+        objectOutputStream.writeObject(instance);
+        File file = new File("singleton_file");
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+        HungrySingleton newInstance = (HungrySingleton) objectInputStream.readObject();
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
+
     }
 }
